c8f2226a3f53ca4257642ef8ffeae3083cb68727
owieth <60756360+owieth@users.noreply.github.com>
2022-04-15 21:14:57 +0200

    feat(snackbar): Add global snackbar class #46

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -3,4 +3,7 @@ import 'package:flutter/material.dart';
 class CustomColors {
   static const Color primaryColor = Color.fromARGB(255, 247, 148, 109);
   static const Color secondaryColor = Color.fromARGB(255, 53, 141, 204);
+  static const Color successSnackBarColor = Colors.green;
+  static const Color infoSnackBarColor = Colors.grey;
+  static const Color errorSnackBarColor = Colors.red;
 }
---
+++ lib/util/snackbar.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class GlobalSnackBar {
+  final String message;
+  final Color color;
+
+  const GlobalSnackBar({
+    required this.color,
+    required this.message,
+  });
+
+  static show(
+    BuildContext context,
+    String message,
+    Color color,
+  ) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+          elevation: 30.0,
+          behavior: SnackBarBehavior.floating,
+          backgroundColor: color,
+          content: Text(message,
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.bodyText1),
+          duration: const Duration(seconds: 3),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(24),
+          ),
+          width: 200),
+    );
+  }
+}
