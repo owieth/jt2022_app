9f072e83e6cb570ac8519455b74f8675f59d4065
Olivier <60756360+owieth@users.noreply.github.com>
2022-05-07 19:59:39 +0200

    feat(location): Add panel workshop layout #98 (#102)

--- lib/screens/map/location.dart
+++ lib/screens/map/location.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_svprogresshud/flutter_svprogresshud.dart';
+import 'package:grouped_list/grouped_list.dart';
+import 'package:jt2022_app/models/workshop.dart';
+import 'package:jt2022_app/services/workshops/workshops_service.dart';
 import 'package:latlong2/latlong.dart' as lat_lng;
-import 'package:line_icons/line_icons.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
+import 'package:collection/collection.dart';
 
 class Location extends StatefulWidget {
   const Location({Key? key}) : super(key: key);
@@ -13,16 +17,22 @@ class Location extends StatefulWidget {
 }
 
 class _LocationState extends State<Location> {
-  final double _initFabHeight = 120.0;
-  double _buttonPositon = 0;
+  int _selectedMarkerIndex = -1;
   double _maxPanelHeight = 0;
 
-  @override
-  void initState() {
-    super.initState();
+  final mapStyle =
+      'https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token={accessToken}';
+  final accessToken =
+      'pk.eyJ1Ijoib3J0ZXhoZCIsImEiOiJjbDFsMmZ3N2UwMWthM2NxcjY3cGFvNjJ2In0.-4xH_hTNDBZQVwGvdY0-UQ';
 
-    _buttonPositon = _initFabHeight;
-  }
+  final List _elements = [
+    {'workshopName': 'Workshop 1', 'floor': 'EG'},
+    {'workshopName': 'Workshop 2', 'floor': 'EG'},
+    {'workshopName': 'Workshop 3', 'floor': '1. OG'},
+    {'workshopName': 'Workshop 4', 'floor': '1. OG'},
+    {'workshopName': 'Workshop 5', 'floor': '2. OG'},
+    {'workshopName': 'Workshop 6', 'floor': '2. OG'},
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -31,7 +41,7 @@ class _LocationState extends State<Location> {
     return Material(
       child: Stack(
         alignment: Alignment.bottomRight,
-        children: <Widget>[
+        children: [
           SlidingUpPanel(
             maxHeight: _maxPanelHeight,
             minHeight: 100,
@@ -45,33 +55,6 @@ class _LocationState extends State<Location> {
               topLeft: Radius.circular(20.0),
               topRight: Radius.circular(20.0),
             ),
-            onPanelSlide: (double pos) => setState(() => _buttonPositon =
-                pos * (_maxPanelHeight - 95.0) + _initFabHeight),
-          ),
-          Positioned(
-            right: 15.0,
-            bottom: _buttonPositon,
-            child: SizedBox(
-              height: 50,
-              child: ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.white,
-                  onPrimary: Colors.black,
-                  shape: const CircleBorder(),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 2.0, bottom: 2.0),
-                  child: RotationTransition(
-                    turns: const AlwaysStoppedAnimation(45 / 360),
-                    child: Icon(
-                      LineIcons.locationArrow,
-                      color: Theme.of(context).primaryColor,
-                    ),
-                  ),
-                ),
-              ),
-            ),
           ),
         ],
       ),
@@ -84,13 +67,13 @@ class _LocationState extends State<Location> {
       removeTop: true,
       child: ListView(
         controller: scrollController,
-        children: <Widget>[
+        children: [
           const SizedBox(
             height: 12.0,
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
+            children: [
               Container(
                 width: 30,
                 height: 5,
@@ -103,12 +86,20 @@ class _LocationState extends State<Location> {
               ),
             ],
           ),
-          const SizedBox(
-            height: 18.0,
+          FutureBuilder(
+            future: WorkshopsService().workshops,
+            builder:
+                (BuildContext context, AsyncSnapshot<List<Workshop>> snapshot) {
+              if (snapshot.hasData) {
+                SVProgressHUD.dismiss();
+                List<Workshop> workshops = snapshot.data!;
+                return _buildWorkshops(workshops);
+              }
+
+              SVProgressHUD.show();
+              return Container();
+            },
           ),
-          _buildWorkshops(),
-          _buildWorkshops(),
-          _buildWorkshops(),
         ],
       ),
     );
@@ -117,23 +108,17 @@ class _LocationState extends State<Location> {
   Widget _buildMap() {
     return FlutterMap(
       options: MapOptions(
-        minZoom: 5,
-        maxZoom: 18,
+        minZoom: 17.75,
+        maxZoom: 17.75,
         zoom: 17.75,
         center: lat_lng.LatLng(46.663370, 7.275294),
-        // bounds: LatLngBounds(
-        //   lat_lng.LatLng(46.661362, 7.271638),
-        //   lat_lng.LatLng(46.665130, 7.279211),
-        // ),
+        swPanBoundary: lat_lng.LatLng(46.662652, 7.273872),
+        nePanBoundary: lat_lng.LatLng(46.664910, 7.278258),
       ),
       nonRotatedLayers: [
         TileLayerOptions(
-          urlTemplate:
-              'https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token={accessToken}',
-          additionalOptions: {
-            'accessToken':
-                'pk.eyJ1Ijoib3J0ZXhoZCIsImEiOiJjbDFsMmZ3N2UwMWthM2NxcjY3cGFvNjJ2In0.-4xH_hTNDBZQVwGvdY0-UQ'
-          },
+          urlTemplate: mapStyle,
+          additionalOptions: {'accessToken': accessToken},
         ),
         MarkerLayerOptions(markers: _buildMapMarkers()),
       ],
@@ -142,20 +127,23 @@ class _LocationState extends State<Location> {
 
   List<Marker> _buildMapMarkers() {
     List<lat_lng.LatLng> coordinates = [
-      lat_lng.LatLng(46.663370, 7.275294),
-      lat_lng.LatLng(46.662973, 7.275370),
-      lat_lng.LatLng(46.663235, 7.276022),
+      lat_lng.LatLng(46.663960, 7.275372),
+      lat_lng.LatLng(46.663920, 7.276075),
+      lat_lng.LatLng(46.663454, 7.274467),
+      lat_lng.LatLng(46.663050, 7.275300),
+      lat_lng.LatLng(46.663300, 7.275963),
     ];
 
     return coordinates
-        .map(
-          (location) => Marker(
-            height: 50,
+        .mapIndexed(
+          (index, location) => Marker(
+            height: _selectedMarkerIndex == index ? 60 : 50,
             width: 50,
             point: location,
             builder: (_) => InkWell(
-              //onTap: () => _pageController.animateToPage(page, duration: duration, curve: curve),
-              onTap: () {},
+              onTap: () => setState(() {
+                _selectedMarkerIndex = index;
+              }),
               child: SvgPicture.asset(
                 'assets/icon/marker.svg',
               ),
@@ -165,35 +153,69 @@ class _LocationState extends State<Location> {
         .toList();
   }
 
-  _buildWorkshops() {
+  Widget _buildWorkshops(List<Workshop> workshop) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 35, vertical: 20),
-      child: SizedBox(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: Image.asset(
-                'assets/images/church.jpeg',
-                height: 75,
-              ),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Text(
-                  'Bern Süd',
-                  style: Theme.of(context).textTheme.bodyText1,
-                ),
-                Text(
-                  'Bümpliz',
-                  style: Theme.of(context).textTheme.bodyText1,
-                ),
-              ],
-            ),
-          ],
+      child: GroupedListView<dynamic, String>(
+        shrinkWrap: true,
+        elements: _elements,
+        groupBy: (element) => element['floor'],
+        // groupComparator: (currentFloor, previousFloor) =>
+        //     previousFloor.compareTo(currentFloor),
+        // itemComparator: (currentFloor, previousFloor) =>
+        //     currentFloor['workshopName']
+        //         .compareTo(previousFloor['workshopName']),
+        //order: GroupedListOrder.ASC,
+        groupSeparatorBuilder: (String value) => Padding(
+          padding: const EdgeInsets.symmetric(vertical: 30.0),
+          child: Text(
+            value,
+            style: Theme.of(context).textTheme.subtitle1,
+          ),
         ),
+        separator: const SizedBox(height: 20),
+        itemBuilder: (c, element) {
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.asset(
+                  'assets/images/church.jpeg',
+                  height: 75,
+                ),
+              ),
+              Text(
+                element['workshopName'],
+                style: Theme.of(context).textTheme.bodyText1,
+              ),
+              // SizedBox(
+              //   height: 100,
+              //   width: 100,
+              //   child: CachedNetworkImage(
+              //     imageUrl: workshop.image,
+              //     imageBuilder: (context, imageProvider) => Container(
+              //       decoration: BoxDecoration(
+              //         borderRadius: BorderRadius.circular(25.0),
+              //         image: DecorationImage(
+              //           image: imageProvider,
+              //           fit: BoxFit.cover,
+              //         ),
+              //       ),
+              //     ),
+              //   ),
+              // ),
+              // Container(
+              //   constraints: const BoxConstraints(maxWidth: 100),
+              //   child: SingleChildScrollView(
+              //     child: Text(workshop.name,
+              //         overflow: TextOverflow.ellipsis,
+              //         style: Theme.of(context).textTheme.bodyText1),
+              //   ),
+              // ),
+            ],
+          );
+        },
       ),
     );
   }
