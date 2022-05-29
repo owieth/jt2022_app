722a56c8c3657115f3211aac08852fefee3987b4
owieth <60756360+owieth@users.noreply.github.com>
2022-05-29 21:17:07 +0200

    feat(deadline): add deadline #121

---
+++ lib/util/deadline.dart
@@ -0,0 +1,10 @@
+class Deadline {
+  bool isDeadline() {
+    DateTime now = DateTime.now();
+    DateTime deadline = DateTime(2022, 07, 24, 12, 00, 00);
+
+    if (now.isAtSameMomentAs(deadline) || now.isAfter(deadline)) return true;
+
+    return false;
+  }
+}
