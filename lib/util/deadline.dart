6eb9a3d0d23d651bf9b0b86cb1983d289b3312d0
owieth <60756360+owieth@users.noreply.github.com>
2022-07-24 15:26:52 +0200

    feat(deadline): new deadline @ 31th of july

--- lib/util/deadline.dart
+++ lib/util/deadline.dart
@@ -1,7 +1,7 @@
 class Deadline {
   bool isDeadline() {
     DateTime now = DateTime.now();
-    DateTime deadline = DateTime(2022, 07, 24, 12, 00, 00);
+    DateTime deadline = DateTime(2022, 07, 31, 12, 00, 00);
 
     if (now.isAtSameMomentAs(deadline) || now.isAfter(deadline)) return true;
 
